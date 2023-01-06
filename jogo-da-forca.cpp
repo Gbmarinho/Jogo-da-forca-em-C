9c5b48afca77d2427b605ff053333a953fcbfb48
Gabriel Marinho <gabriel.marinho@injunior.com.br>
2023-01-06 16:38:23 -0300

    finalizando jogo da forca, podendo jogar em dupla

--- jogo-da-forca.cpp
+++ jogo-da-forca.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <Windows.h>
 #include <iostream>
 #include <time.h>
 
@@ -44,7 +45,8 @@ void jogarSozinho(){
     int tentativas = 0, max_tentativas = tamPalavra+5;
     int cont = 0;
     char letra;
-    string letras_arriscadas;
+    string letras_arriscadas = "";
+    string arrisca_palavra;
     bool ja_digitada;
 
 
@@ -56,9 +58,21 @@ void jogarSozinho(){
         ja_digitada = false;
         printf("Palavra: %s (Tamanho: %d)", palavra_mascara.c_str(), tamPalavra);
         printf("\nTentativas restantes: %d", (max_tentativas - tentativas));
-        printf("\nLetras arriscadas: %s", letras_arriscadas.c_str());
-        printf("\nDigite uma letra: ");
+        printf("\nLetras arriscadas: %s\n", letras_arriscadas.c_str());
+        printf("Digite uma letra (ou digite 1 para arriscar a palavra): ");
         scanf("%c", &letra);
+        if(letra == '1'){
+            fflush(stdin);
+            printf("\nEscreva a palavra: ");
+            cin >> arrisca_palavra;
+            if(arrisca_palavra == palavra){
+                palavra_mascara = arrisca_palavra;
+            }else{
+                tentativas = max_tentativas;
+            }
+            fflush(stdin);
+
+        }
         for(cont=0; cont<tentativas*2;cont++){
             if(letras_arriscadas[cont]==letra){
                 ja_digitada = true;
@@ -81,13 +95,89 @@ void jogarSozinho(){
     if(palavra_mascara == palavra){
         limpaTela();
         printf("Parabans voce ganhou!!");
+        Sleep(2000);
     }else{
         limpaTela();
         printf("Que pena, voce perdeu");
+        Sleep(2000);
     }
 
 }
 
+void jogarDupla(){
+    limpaTela();
+    string palavra;
+    printf("Bem vindo ao jogo em dupla!");
+    printf("\nUm dos dois irá digitar uma palavra para o outro tentar acertar.");
+    printf("\nDigite uma palavra: ");
+    cin >> palavra;
+    fflush(stdin);
+    int tamPalavra = tamanho_da_palavra(palavra);
+
+    string palavra_mascara = retorna_palavra_mascara(tamPalavra);
+
+
+    int tentativas = 0, max_tentativas = tamPalavra+5;
+    int cont = 0;
+    char letra;
+    string letras_arriscadas = "";
+    string arrisca_palavra;
+    bool ja_digitada;
+
+
+    while(palavra != palavra_mascara && max_tentativas - tentativas > 0){
+        limpaTela();
+        if(ja_digitada == true){
+            printf("Letra ja foi digitada, tente outra letra diferente\n");
+        }
+        ja_digitada = false;
+        printf("Palavra: %s (Tamanho: %d)", palavra_mascara.c_str(), tamPalavra);
+        printf("\nTentativas restantes: %d", (max_tentativas - tentativas));
+        printf("\nLetras arriscadas: %s\n", letras_arriscadas.c_str());
+        printf("Digite uma letra (ou digite 1 para arriscar a palavra): ");
+        scanf("%c", &letra);
+        if(letra == '1'){
+            fflush(stdin);
+            printf("\nEscreva a palavra: ");
+            cin >> arrisca_palavra;
+            if(arrisca_palavra == palavra){
+                palavra_mascara = arrisca_palavra;
+            }else{
+                tentativas = max_tentativas;
+            }
+            fflush(stdin);
+
+        }
+        for(cont=0; cont<tentativas*2;cont++){
+            if(letras_arriscadas[cont]==letra){
+                ja_digitada = true;
+            }
+        }
+        if(ja_digitada == false){
+            letras_arriscadas += letra;
+            letras_arriscadas += " ";
+            for(cont=0;cont<tamPalavra;cont++){
+                if(palavra[cont] == letra){
+                    palavra_mascara[cont] = palavra[cont];
+                }
+            }
+            tentativas = tentativas +1;
+        }
+
+
+        fflush(stdin);
+    }
+    if(palavra_mascara == palavra){
+        limpaTela();
+        printf("Parabans voce ganhou!!");
+        Sleep(2000);
+    }else{
+        limpaTela();
+        printf("Que pena, voce perdeu");
+        Sleep(2000);
+    }
+
+}
 
 void menuInicial(){
 
@@ -95,7 +185,7 @@ void menuInicial(){
     int opcao = 0;
 
     //Enquanto o jogador não digita uma opcao válida, mostra o menu novamente
-    while(opcao < 1 || opcao > 3){
+    while(opcao < 1 || opcao > 4){
         limpaTela();
         printf("Bem vindo ao Jogo");
         printf("\n1 - Jogar Sozinho");
@@ -107,19 +197,17 @@ void menuInicial(){
         //Faz a leitura da opcao
         scanf("%d", &opcao);
 
+        fflush(stdin);
         //Faz algo de acordo com a opcao escolhida
         switch(opcao){
             case 1:
                 //Inicia o jogo
-                if(1 == 1){
-                    menuInicial();
-                }
+                jogarSozinho();
+                menuInicial();
                 break;
             case 2:
-                //Inicia o jogo
-                if(1 == 1){
-                    menuInicial();
-                }
+                jogarDupla();
+                menuInicial();
                 break;
             case 3:
                 //Mostra informacoes do Jogo
@@ -128,6 +216,7 @@ void menuInicial(){
                 printf("Jogo desenvolvido por Gabriel em 2023");
                 printf("\n1 - Voltar");
                 printf("\n2 - Sair");
+                printf("\nEscolha uma opcao e tecle ENTER:");
                 scanf("%d", &opcao);
                 if(opcao == 1){
                     menuInicial();
@@ -135,6 +224,7 @@ void menuInicial(){
 
                 break;
             case 4:
+                limpaTela();
                 printf("Ate mais!");
                 break;
         }
@@ -146,6 +236,6 @@ int main(){
     //Para gerar números realmente aleatórios
     srand((unsigned)time(NULL));
 
-    jogarSozinho();
+    menuInicial();
     return 0;
 }
